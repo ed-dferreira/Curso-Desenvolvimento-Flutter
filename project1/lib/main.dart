7f498d43ed981b5c690d4794f2ab618675727f52
Eduardo <eduardoodferreiradev@gmail.com>
2024-09-03 14:29:53 -0300

    fuçando os widgets

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const AppWidget(title: 'baco bacoso'));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeWidget(),
+    );
+  }
 }
 
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({Key? key, required this.title}) : super(key: key);
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black, // Added background color for visibility
-      child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: const TextStyle(color: Colors.white, fontSize: 40.0),
-        ),
+      child: MyWidget(),
+    );
+  } // Esta chave estava faltando para fechar a classe HomeWidget
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  Color color = Colors.red;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          print("Click");
+          color = (color == Colors.blue) ? Colors.red : Colors.blue;
+        });
+      },
+      child: Container(
+        color: color,
       ),
     );
   }
